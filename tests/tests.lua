c24bda487bea5cfdee9d45a2465e545c42081b8c
NickJ202 <nickjuliano20@gmail.com>
2024-09-12 20:45:59 -0400

    utils test runner

--- tests/tests.lua
+++ tests/tests.lua
@@ -3,6 +3,41 @@ package.path = package.path .. ';../src/?.lua'
 local ucm = require('ucm')
 local utils = require('utils')
 
+utils.test('Create listing',
+	function()
+		Orderbook = {}
+
+		ucm.createOrder({
+			orderId = 'N5vr71SXaEYsdVoVCEB5qOTjHNwyQVwGvJxBh_kgTbE',
+			dominantToken = 'LGWN8g0cuzwamiUWFT7fmCZoM4B2YDZueH9r8LazOvc',
+			swapToken = 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10',
+			sender = 'SaXnsUgxJLkJRghWQOUs9-wB0npVviewTkUbh2Yk64M',
+			quantity = 1000,
+			price = '500000000000',
+			timestamp = '1722535710966',
+			blockheight = '123456789',
+		})
+
+		return Orderbook
+	end,
+	{
+		{
+			Pair = { 'LGWN8g0cuzwamiUWFT7fmCZoM4B2YDZueH9r8LazOvc', 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10' },
+			Orders = {
+				{
+					Creator = 'SaXnsUgxJLkJRghWQOUs9-wB0npVviewTkUbh2Yk64M',
+					DateCreated = '1722535710966',
+					Id = 'N5vr71SXaEYsdVoVCEB5qOTjHNwyQVwGvJxBh_kgTbE',
+					OriginalQuantity = '1000',
+					Price = '500000000000',
+					Quantity = '1000',
+					Token = 'LGWN8g0cuzwamiUWFT7fmCZoM4B2YDZueH9r8LazOvc'
+				}
+			},
+		},
+	}
+)
+
 utils.test('Single order fully matched',
 	function()
 		Orderbook = {
@@ -222,6 +257,58 @@ utils.test('Single order partially matched (denominated)',
 					}
 				},
 				Vwap = '500000000000',
+				Block = '123456789',
+				DominantToken = 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10'
+			}
+		}
+	}
+)
+
+utils.test('Single order fully matched (denominated / fractional)',
+	function()
+		Orderbook = {
+			{
+				Pair = { 'DM3FoZUq_yebASPhgd8pEIRIzDW6muXEhxz5-JwbZwo', 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10' },
+				Orders = {
+					{
+						Creator = 'LNtQf8SGZbHPeoksAqnVKfZvuGNgX4eH-xQYsFt_w-k',
+						DateCreated = '1722535710966',
+						Id = 'N5vr71SXaEYsdVoVCEB5qOTjHNwyQVwGvJxBh_kgTbE',
+						OriginalQuantity = '1',
+						Price = '50000000',
+						Quantity = '1',
+						Token = 'DM3FoZUq_yebASPhgd8pEIRIzDW6muXEhxz5-JwbZwo'
+					}
+				},
+			},
+		}
+
+		ucm.createOrder({
+			orderId = tostring(1),
+			dominantToken = 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10',
+			swapToken = 'DM3FoZUq_yebASPhgd8pEIRIzDW6muXEhxz5-JwbZwo',
+			sender = 'User' .. tostring(1),
+			quantity = tostring(50000000),
+			timestamp = os.time() + 1,
+			blockheight = '123456789',
+			transferDenomination = '1000000'
+		})
+
+		return Orderbook
+	end,
+	{
+		{
+			Pair = { 'DM3FoZUq_yebASPhgd8pEIRIzDW6muXEhxz5-JwbZwo', 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10' },
+			Orders = {},
+			PriceData = {
+				MatchLogs = {
+					{
+						Quantity = '1',
+						Price = '50000000',
+						Id = 'N5vr71SXaEYsdVoVCEB5qOTjHNwyQVwGvJxBh_kgTbE'
+					}
+				},
+				Vwap = '50000000',
 				Block = '123456789',
 				DominantToken = 'xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10'
 			}

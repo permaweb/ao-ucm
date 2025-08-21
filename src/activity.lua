8c61e25dbe402eec11a52d66ba0ca525f72b8608
Bartosz Solka <bartosz.solka@blockydevs.com>
2025-08-21 10:59:00 +0200

    AM-60 Added handlers for listed and cancelled orders

--- src/activity.lua
+++ src/activity.lua
@@ -11,6 +11,25 @@ if not CancelledOrders then CancelledOrders = {} end
 if not SalesByAddress then SalesByAddress = {} end
 if not PurchasesByAddress then PurchasesByAddress = {} end
 
+-- Get listed orders
+Handlers.add('Get-Listed-Orders', Handlers.utils.hasMatchingTag('Action', 'Get-Listed-Orders'), function(msg)
+	local page = utils.parsePaginationTags(msg)
+
+	local ordersArray = {}
+	for _, order in pairs(ListedOrders) do
+		local orderCopy = utils.deepCopy(order)
+		table.insert(ordersArray, orderCopy)
+	end
+
+	local paginatedOrders = utils.paginateTableWithCursor(ordersArray, page.cursor, page.cursorField, page.limit, page.sortBy, page.sortOrder, page.filters)
+
+	ao.send({
+		Target = msg.From,
+		Action = 'Read-Success',
+		Data = json.encode(paginatedOrders)
+	})
+end)
+
 -- Get executed orders
 Handlers.add('Get-Executed-Orders', Handlers.utils.hasMatchingTag('Action', 'Get-Executed-Orders'), function(msg)
 	local page = utils.parsePaginationTags(msg)
@@ -30,6 +49,24 @@ Handlers.add('Get-Executed-Orders', Handlers.utils.hasMatchingTag('Action', 'Get
 	})
 end)
 
+-- Get cancelled orders
+Handlers.add('Get-Cancelled-Orders', Handlers.utils.hasMatchingTag('Action', 'Get-Cancelled-Orders'), function(msg)
+	local page = utils.parsePaginationTags(msg)
+
+	local ordersArray = {}
+	for _, order in pairs(CancelledOrders) do
+		local orderCopy = utils.deepCopy(order)
+		table.insert(ordersArray, orderCopy)
+	end
+
+	local paginatedOrders = utils.paginateTableWithCursor(ordersArray, page.cursor, page.cursorField, page.limit, page.sortBy, page.sortOrder, page.filters)
+
+	ao.send({
+		Target = msg.From,
+		Action = 'Read-Success',
+		Data = json.encode(paginatedOrders)
+	})
+end)
 
 
 -- Read activity
